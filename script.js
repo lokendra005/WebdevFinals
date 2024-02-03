4b4923c67ab2e51a586de270b2a373be04b5d392
lokendra005 <141302553+lokendra005@users.noreply.github.com>
2024-02-03 18:21:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("connected");
+
+let post_btn = document.querySelector("#post-btn")
+
+post_btn.addEventListener('click',(e)=>{
+    let posts_box = document.querySelector(".posts") //posts box
+    let main_post = document.createElement('div');
+    main_post.innerHTML = 
+    `<div class="main-post">
+    <div class="post-main">
+        <div class="prof-img">
+        </div>
+        <div class="post-right">    
+            <div class="post-det">
+                <div class="post-right-btns">
+                    <img src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/031/original/profile_image.png?1706888739" alt="#">
+                    <h4>notloki @loki</h4>
+                </div>
+            </div>
+            
+            <div class="post-txt-area">
+                <textarea name="post" id="post-area-text" cols="30" rows="10" disabled = "disabled"></textarea>
+            </div>
+            
+            <div class="like-cmnt-btns">
+                <img src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/026/original/comment.png?1706888619" alt="#">
+                <img src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/029/original/heart.png?1706888679" alt="#">
+            </div>
+        </div>
+    </div>
+    </div>`
+
+    posts_box.appendChild(main_post);
+
+})
+
+
+let post_area = document.querySelector("#post-area");
+console.log(post_area.getAttribute['value'])
